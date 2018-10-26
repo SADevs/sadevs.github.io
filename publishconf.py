b41f62bcc8d765eadb9edfe62464aeb46e3615e2
Daniel Wallace <danielwallace@gtmanfred.com>
2018-10-25 19:50:38 -0500

    fix site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://sanantoniodevs.com'
+SITEURL = 'https://blog.sanantoniodevs.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
